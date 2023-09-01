60988b658806648ec57477f1ba98190989100a31
Alisa Promthep <alisa.promthep@gmail.com>
2023-08-31 23:22:55 -0700

    added rules

---
+++ src/app/data/rules.js
@@ -0,0 +1,38 @@
+export const rules = [
+    {
+    'rule':'must contain at least 5 letters',
+    'answers':['']
+},
+{
+    'rule':'must contain 2 capital letter',
+    'answers':['']
+},
+{
+    'rule':'must contain at least 2 numbers',
+    'answers':[0-9]
+},
+{
+    'rule':'must contain a square root of 81',
+    'answers':[9]
+},
+{
+    'rule':'must contain perodic symbol of two main elements in the air',
+    'answers':['N', 'O']
+},
+{
+    'rule':"must contain einstein's famous energy equation",
+    'answers':['e=mc2']
+},
+{
+    'rule':'must contain atomic mass of the element that make up diamond (rounded to the nearest tenth)',
+    'answers':[12]
+},
+{
+    'rule':'must contain a number in the citric acid pH range',
+    'answers':[3-6]
+},
+{
+    'rule':'numbers must add up to 32',
+    'answers':[32]
+}
+];
--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,14 +1,41 @@
-import React from 'react';
+"use client"
+
+import React, {useState} from 'react';
+import {rules} from '../data/rules';
+
 
 function Gamepage() {
+
+    const [password, setPassword] = useState('');
+
+    const checkPass = (current)=>{
+
+
+    }
+
+    const handleChange = (e)=>{
+        e.preventDefault();
+
+        const value = e.target.value; 
+
+        setPassword((prev)=>{
+            return (
+                prev + value
+            )
+        })
+
+        console.log(password)
+    }
+
     return (
-        <div>
+        <div className='flex flex-col justify-center items-center'>
             <h1 className='capitalize'>password game</h1>
             <h2 className='capitalize'>science addition</h2>
             <textarea
             type='text'
             name='password'
             className='rounded-md h-8 text-black'
+            onChange={handleChange}
             />
         </div>
     )
