5dde64186542aefb0b39f871697e38d2178b7efb
Alisa Promthep <alisa.promthep@gmail.com>
2023-09-05 14:51:38 -0700

    first rule, check successful, and move on to second (then error)

--- src/app/data/rules.js
+++ src/app/data/rules.js
@@ -1,52 +1,60 @@
 export const rules = [
-    {
+    {'id':1,
     'rule':'must contain at least 5 letters',
-    'answers':'/(?=.{5,})/g',
+    'answers':'(?=.{5,})',
     'add':false,
     'done':false,
 },
 {
+    'id':2,
     'rule':'must contain 2 capital letter',
-    'answers':[''],
+    'answers':'(\w*[A-Z]\w*[A-Z]\w*)',
     'add':false,
     'done':false,
 },
 {
+    'id':3,
     'rule':'must contain at least 2 numbers',
     'answers':[0-9],
     'add':false,
     'done':false,
 },
 {
+    'id':4,
     'rule':'must contain a square root of 81',
     'answers':[9],
     'add':false,
     'done':false,
 },
 {
+    'id':5,
     'rule':'must contain perodic symbol of two main elements in the air',
     'add':false,
     'answers':['N', 'O']
 },
 {
+    'id':6,
     'rule':"must contain einstein's famous energy equation",
     'answers':['e=mc2'],
     'add':false,
     'done':false,
 },
 {
+    'id':7,
     'rule':'must contain atomic mass of the element that make up diamond (rounded to the nearest tenth)',
     'answers':[12],
     'add':false,
     'done':false,
 },
 {
+    'id':8,
     'rule':'must contain a number in the citric acid pH range',
     'answers':[3-6],
     'add':false,
     'done':false,
 },
 {
+    'id':9,
     'rule':'numbers must add up to 32',
     'answers':[32],
     'add':false,
--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,55 +1,79 @@
 "use client"
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {rules} from '../data/rules';
+import { current } from 'tailwindcss/colors';
 
 
 function Gamepage() {
 
     const [password, setPassword] = useState('');
     const [show, setShow] = useState([]);
+    const [currentIndex, setCurrentId] = useState(0);
+    const [oneValidate, setOneValidate] = useState(false);
+    const [twoValidate, setTwoValidate] = useState(false);
+    const [threeValidate, setThreeValidate] = useState(false);
+    const [fourValidate, setFourValidate] = useState(false);
+    const [fiveValidate, setFiveValidate] = useState(false);
+    const [sixValidate, setSixValidate] = useState(false);
+    const [sevenValidate, setSevenValidate] = useState(false);
+    const [eightValidate, setEightValidate] = useState(false);
+    const [nineValidate, setNineValidate] = useState(false);
 
-    const addShow = (num)=>{
+    
+
+    // useEffect(()=>{
+
         
-        const currentRule = show[num];
-        if(show.length && currentRule.add){
+    // },[currentId] )
+
+    const addShow = (id)=>{
+        
+        if(show.length && show[id].add){
             return;
         } else {
             //set show state for that rule to be true
-            setShow(prev=> [...prev,
-                {...rules[num], 
-                add:true}
+            setShow(prev=> [
+                {...rules[id], 
+                add:true, ...prev}
             ] )
         }
     };
 
     const createCheck = (str)=> {return new RegExp(str)};
 
-    const updateState = (num,bool)=>{
-        setShow((prev)=>[...prev,show[num].done=[bool]]);
+    const updateState = (index,bool)=>{
+        console.log(show[index].id)
+        console.log('show in updateState',show);
+        setShow((prev)=>[...prev,show[index].done=[bool]]);
     };
 
-    const check = (num,str)=>{
+    const check = (str)=>{
         if (show.length) {
-            const answer = createCheck(show[num].answers);
-            console.log('ans',answer)
-            //check 
-            return answer.test(str)
+            show.map((r,i)=>{
+                const answer = createCheck(r.answers);
+                //check 
+                const checkResult = answer.test(str);
+                if(checkResult && r.done === false){
+                    setCurrentId((prev)=> prev+1);
+                    updateState(i,checkResult)
+                    
+                }
+            })
         } 
 
-    }
+    };
 
     const handleChange = (e)=>{
         e.preventDefault();
-        addShow(0);
+        addShow(currentIndex);
         
         const value = e.target.value; 
         
-        console.log(check(0,value));
+        const checkResult = check(value);
+        //console.log(checkResult);
 
         setPassword(value)
-
-        console.log(password)
     };
 
     return (
